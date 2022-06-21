8dd42201e8f0fc7bbc21670e5a436cb027e0e46e
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-06-21 10:34:16 -0300

    Metric service basic created

---
+++ Metrics.py
@@ -0,0 +1,38 @@
+class Metrics:
+    def __init__(self):
+        self.map_metrics = {}
+
+    def get_metrics(self):
+        metrics = ""
+        for metric_name in self.map_metrics:
+            metric_ = self.map_metrics[metric_name]
+            metric_id = "{application=\"" + str(metric_['metric'][0]['appName']) + "\"" \
+                        + ", method=\"" + str(metric_['metric'][0]['method']) + "\"" \
+                        + ", class=\"" + str(metric_['metric'][0]['fullClassName']) + "\"" \
+                        + "} "
+            metrics += "execution_time_seconds_sum" + metric_id + str(metric_['sum'] / 1000) + "\n"
+            metrics += "execution_time_seconds_avg" + metric_id + str(metric_['avg'] / 1000) + "\n"
+            metrics += "execution_time_seconds_max" + metric_id + str(metric_['max'] / 1000) + "\n"
+            metrics += "execution_time_seconds_count" + metric_id + str(metric_['count']) + "\n"
+        return metrics
+
+    def add_metric(self, metric):
+        metric_name = metric['appName'] + "_" + \
+                      metric['fullClassName'] + "_" + metric['method']
+        metric_name = metric_name.replace(".", "_").lower()
+        if metric_name in self.map_metrics:
+            metrics = self.map_metrics[metric_name]
+            metrics['sum'] += metric['executionTime']
+            metrics['count'] += 1
+            metrics['avg'] = metrics['sum'] / metrics['count']
+            metrics['max'] = max(metrics['max'], metric['executionTime'])
+            metrics['min'] = min(metrics['min'], metric['executionTime'])
+            metrics['last'] = metric['executionTime']
+            metrics['metric'].append(metric)
+            self.map_metrics[metric_name] = metrics
+        else:
+            metrics = {'sum': metric['executionTime'], 'count': 1, 'avg': metric['executionTime'],
+                       'max': metric['executionTime'], 'min': metric['executionTime'],
+                       'last': metric['executionTime'],
+                       'metric': [metric]}
+            self.map_metrics[metric_name] = metrics
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from prometheus_flask_exporter import PrometheusMetrics
+
+from Metrics import Metrics
+
+app = Flask(__name__)
+
+service_metrics = Metrics()
+metrics = PrometheusMetrics(app, group_by='endpoint')
+
+
+@app.route('/services-metrics', methods=['GET'])
+def get_metrics():
+    get_metrics = service_metrics.get_metrics()
+    return str(get_metrics), 200, {'Content-Type': 'text/plain'}
+
+
+@app.route('/metric', methods=['POST'])
+def post_metrics():
+    json = request.get_json()
+    service_metrics.add_metric(json)
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
