ce2c4754de6d4c5358261e80a563532c76a574bd
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-06-23 11:33:05 -0300

    Improvement

--- Metrics.py
+++ Metrics.py
@@ -6,9 +6,9 @@ class Metrics:
         metrics = ""
         for metric_name in self.map_metrics:
             metric_ = self.map_metrics[metric_name]
-            metric_id = "{application=\"" + str(metric_['metric'][0]['appName']) + "\"" \
-                        + ", method=\"" + str(metric_['metric'][0]['method']) + "\"" \
-                        + ", class=\"" + str(metric_['metric'][0]['fullClassName']) + "\"" \
+            metric_id = "{application=\"" + str(metric_['metric']['appName']) + "\"" \
+                        + ", method=\"" + str(metric_['metric']['method']) + "\"" \
+                        + ", class=\"" + str(metric_['metric']['fullClassName']) + "\"" \
                         + "} "
             metrics += "execution_time_seconds_sum" + metric_id + str(metric_['sum'] / 1000) + "\n"
             metrics += "execution_time_seconds_avg" + metric_id + str(metric_['avg'] / 1000) + "\n"
@@ -28,11 +28,11 @@ class Metrics:
             metrics['max'] = max(metrics['max'], metric['executionTime'])
             metrics['min'] = min(metrics['min'], metric['executionTime'])
             metrics['last'] = metric['executionTime']
-            metrics['metric'].append(metric)
+            metrics['metric'] = metric
             self.map_metrics[metric_name] = metrics
         else:
             metrics = {'sum': metric['executionTime'], 'count': 1, 'avg': metric['executionTime'],
                        'max': metric['executionTime'], 'min': metric['executionTime'],
                        'last': metric['executionTime'],
-                       'metric': [metric]}
+                       'metric': metric}
             self.map_metrics[metric_name] = metrics
